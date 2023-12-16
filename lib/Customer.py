4b0bc28ede412a4b00efa8a2e1cd42bebf0cb966
Njugunasam <sammynjuguna5826@gmail.com>
2023-12-16 11:24:45 +0300

    done

--- lib/Customer.py
+++ lib/Customer.py
@@ -1,7 +1,7 @@
 class Customer:
     customers = []
 
-    # Initializes a new Customer instance
+    # Initializing a new Customer instance
     def __init__(self, given_name, family_name):
         self.given_name = given_name
         self.family_name = family_name
